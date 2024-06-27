bb31eead6aa0fd5c8cb0dec7e90a4b37cddb8875
Richard Rauser <richard@raaza.net>
2024-06-27 09:55:14 +0100

    Updating home screen with animated gif and button to create page

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,21 +1,27 @@
-import { Title, Text, Anchor } from '@mantine/core';
+'use client';
+import { Title, Text, Anchor, Button } from '@mantine/core';
 import classes from './Welcome.module.css';
+import Image from 'next/image';
+import { IconArrowRight } from '@tabler/icons-react';
 
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
+      <Title className={classes.title} ta="center" mt="lg" mb="lg">
         Welcome to{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'blue', to: 'orange' }}>
           anglez.xyz
         </Text>
       </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        Create abstract, on-chain NFT art.<br></br>
-        <Anchor href="create" size="lg">
+      <center>
+        <Image src="/anglez-animated-1.gif" alt="anglez" width={300} height={300} />
+        <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="lg">
+          Create abstract, on-chain NFT art.<br></br>
+        </Text>
+        <Button mt="lg" component="a" href="/create" rightSection={<IconArrowRight size={14} />}>
           Get started creating anglez!
-        </Anchor>
-      </Text>
+        </Button>
+      </center>
     </>
   );
 }
