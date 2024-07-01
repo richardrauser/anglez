eaf79861fc5b6356cf9402134eb759ef7696c2b5
Richard Rauser <richard@raaza.net>
2024-07-01 17:09:20 +0200

    about updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,12 +18,13 @@ export default function AboutPage() {
         <h2>how do I use this?</h2>
         <Text>
           From the Connect Wallet button, select the wallet you wish to use. Coinbase Smart Wallet
-          is the easiest option because you won't have to configure anything or even get crypto..
-          transaction fees are sponsorred! First, install a crypto wallet like{' '}
+          is the easiest option because you won't have to configure anything or buy crypto..
+          transaction fees are sponsorred! You can alternatively install a crypto wallet like{' '}
           <a href="https://metamask.io">MetaMask</a> or{' '}
-          <a href="https://www.coinbase.com/wallet">Coinbase Wallet</a>.
+          <a href="https://www.coinbase.com/wallet">Coinbase Wallet</a>, however you will need to
+          setup and fund the wallet yourself.
         </Text>
-        <Text>
+        {/* <Text>
           Next, you need Seploia ETH in order to mint anglez. You can get Sepolia ETH by using a
           Sepolia faucet. <br />
           Try this one: <a href="https://www.sepoliafaucet.io/">
@@ -35,8 +36,8 @@ export default function AboutPage() {
           <a href="https://www.google.com/search?q=Sepolia+faucet">
             https://www.google.com/search?q=sepolia+faucet
           </a>
-        </Text>
-        <Text>Failing that, contact RR and I'll send you some Sepolia ETH to get you started.</Text>
+        </Text> */}
+        {/* <Text>Failing that, contact RR and I'll send you some Sepolia ETH to get you started.</Text>  */}
         <h2>contract</h2>
         <Text>
           The anglez smart contract address on {AnglezCurrentNetworkName} is:{' '}
@@ -44,6 +45,27 @@ export default function AboutPage() {
             {shortenAddress(AnglezContractAddress)}
           </a>
           . <br />
+        </Text>
+        <h2>what tech is anglez built on?</h2>
+        <Text>
+          OnchainKit, Coinbase Smart Wallet, Next.js 14, Vercel, Farcaster Frames, Mantine, React.
+          ethers.js, hardhat
+        </Text>
+        {/* <ul>
+          <li>OnchainKit</li>
+          <li>Coinbase Smart Wallet</li>
+        </ul> */}
+        <h2>who made this?</h2>
+        <Text>
+          I did! 😝 It's{` `}
+          <a target="_blank" href="https://volstrate.com">
+            volstrate
+          </a>
+          , aka{' '}
+          <a target="_blank" href="">
+            Richard Rauser
+          </a>
+          .<br />
         </Text>
       </div>
     </>
