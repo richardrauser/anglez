d1f15ea30201cc2480c01054883e085803b4a8fb
Richard Rauser <richard@raaza.net>
2024-07-07 13:15:02 +0100

    Further updates to About page, version bump

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -50,20 +50,6 @@ export default function AboutPage() {
           Once that's done, visit the <Link href="/create">create page</Link> and get started
           minting anglez!
         </Text>
-        {/* <Text>
-          Next, you need Seploia ETH in order to mint anglez. You can get Sepolia ETH by using a
-          Sepolia faucet. <br />
-          Try this one: <a href="https://www.sepoliafaucet.io/">
-            {' '}
-            https://www.sepoliafaucet.io/
-          </a>{' '}
-          <br />
-          Or search google:{' '}
-          <a href="https://www.google.com/search?q=Sepolia+faucet">
-            https://www.google.com/search?q=sepolia+faucet
-          </a>
-        </Text> */}
-        {/* <Text>Failing that, contact RR and I'll send you some Sepolia ETH to get you started.</Text>  */}
         <h2>anglez smart contract</h2>
         <Text>
           The anglez smart contract address on{' '}
